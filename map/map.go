6fefb0bd6f846ab8930ec97eb5070d8f6386e54d
mdh6789 <mdh6789@gmail.com>
2017-12-07 11:10:47 +0800

    update map.go: remove defer usage

--- map/map.go
+++ map/map.go
@@ -15,27 +15,28 @@ func NewMap() *Map {
 
 func (this *Map) Get(key interface{}) (interface{}, bool) {
 	this.RLock()
-	defer this.RUnlock()
 	val, ok := this.M[key]
+	this.RUnlock()
 	return val, ok
 }
 
 func (this *Map) Set(key interface{}, val interface{}) {
 	this.Lock()
-	defer this.Unlock()
 	this.M[key] = val
+	this.Unlock()
 }
 
 func (this *Map) Len() int {
 	this.RLock()
-	defer this.RUnlock()
-	return len(this.M)
+	length := len(this.M)
+	this.RUnlock()
+	return length
 }
 
 func (this *Map) Delete(key interface{}) {
 	this.Lock()
-	defer this.Unlock()
 	delete(this.M, key)
+	this.Unlock()
 }
 
 func (this *Map) BatchDelete(keys []interface{}) {
@@ -45,8 +46,8 @@ func (this *Map) BatchDelete(keys []interface{}) {
 	}
 
 	this.Lock()
-	defer this.Unlock()
 	for i := 0; i < count; i++ {
 		delete(this.M, keys[i])
 	}
+	this.Unlock()
 }
